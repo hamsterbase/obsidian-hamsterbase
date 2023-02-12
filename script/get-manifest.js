c664a47a78b0f11ff0f695c143eebc5885807d27
monster <monster@hamsterbase.com>
2023-02-12 18:04:32 +0800

    fix: fix plugin version

--- script/get-manifest.js
+++ script/get-manifest.js
@@ -1,11 +1,7 @@
 const manifest = require('../manifest.json');
-const { version } = require('../package.json');
 
 function getManifest() {
-  return {
-    ...manifest,
-    version,
-  };
+  return manifest;
 }
 
 module.exports = getManifest;
